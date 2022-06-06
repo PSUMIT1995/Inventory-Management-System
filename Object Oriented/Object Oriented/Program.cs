f49e635cf942ae4aa541e630cfb7eb4ae1fcf663
PSUMIT1995 <psumit191.sp@gmail.com>
2022-06-06 19:50:16 +0530

    Inventory Mangement System

--- Object Oriented/Object Oriented/Program.cs
+++ Object Oriented/Object Oriented/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace Object_Oriented
+{
+    class Program
+    {
+       public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Inventory Management System!");
+            Inventory.Inventory.JsonDeserilaize();
+        }
+    }
+
+}
